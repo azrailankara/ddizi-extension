89d560a05524297318e2a145e7e71f980d0f71a2
azrailankara <dobbyisfree06>
2025-01-23 15:20:19 +0300

    Update Gradle configuration to fix build issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,27 @@
 plugins {
-    id("com.android.library") version "7.0.4"
-    id("org.jetbrains.kotlin.android") version "1.7.10"
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
 }
 
 android {
@@ -35,18 +56,9 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("com.github.Blatzar:NiceHttp:0.4.3")
     implementation("org.jsoup:jsoup:1.15.3")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+    implementation("com.fasterxml.jackson.module:jackson-kotlin:2.13.1")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.1")
-
-    // cloudstream
-    implementation("com.github.recloudstream:cloudstream:pre-release")
 }
 
-// cloudstream
-apply(from = "https://raw.githubusercontent.com/recloudstream/gradle/master/cloudstream.gradle")
-repositories {
-    google()
-    mavenCentral()
-    maven("https://jitpack.io")
-} 
+apply(from = "https://raw.githubusercontent.com/recloudstream/gradle/master/cloudstream.gradle") 
