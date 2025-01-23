a97adb9dbced5580426505427054368fa3fda9d2
azrailankara <dobbyisfree06>
2025-01-23 14:50:33 +0300

    Update main build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,24 @@
-version = 1
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.lagradost.cloudstream3.gradle") version "2.0"
+}
 
 android {
+    compileSdk = 33
+    
     defaultConfig {
+        minSdk = 21
+        targetSdk = 33
         manifestPlaceholders["CLOUDSTREAM_API_KEY"] = "0123456789"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
 } 
