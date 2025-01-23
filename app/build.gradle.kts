368e6a5c9aa1145fafe60707b1a7ddb53672f2c9
azrailankara <dobbyisfree06>
2025-01-23 14:54:48 +0300

    Update build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,36 @@
-// use an integer for version numbers
-version = 1
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.lagradost.cloudstream3.gradle") version "2.0"
+}
 
 android {
+    compileSdk = 33
+
     defaultConfig {
+        minSdk = 21
+        targetSdk = 33
         // all extensions must have this enabled
         manifestPlaceholders["CLOUDSTREAM_API_KEY"] = "0123456789"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.github.Blatzar:NiceHttp:0.4.3")
+    implementation("org.jsoup:jsoup:1.15.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.1")
 }
 
 cloudstream {
--- build.gradle.kts
+++ build.gradle.kts
@@ -21,4 +21,26 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
 } 
